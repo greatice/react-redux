8713deba171ef381fa97de7e49bed7ff24d7d234
pei luo <alexluolibra@hotmail.com>
2019-09-04 21:00:46 +1000

    redux counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import {useSelector,useDispatch} from 'react-redux';
+import {increment,decrement} from './actions/index';
+
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>
-        Hello
+        Counter : {counter}
       </h1>
+     { isLogged ? <p>If isLogged is true, please show the info.</p> :' If isLogged is false, please show the info.'}
+      <button onClick={()=> dispatch(increment())}>
+        +
+      </button>
+      <button onClick={()=> dispatch(decrement())}>
+        -
+      </button>
+   
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
+import {createStore} from 'redux'; //the function is to create a store used to save the info to communicate between the different reducers(combine together to use)
 import allReducers from './reducers/index';
+import { Provider } from 'react-redux'; //make the store global for the app
+
 
 
 
@@ -12,9 +14,15 @@ import allReducers from './reducers/index';
 const store = createStore(
     allReducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+    ); 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store = {store}> 
+{/* the store in the brace is the store just defined above. */}
+<App />
+</Provider>, 
+
+document.getElementById('root'));
 
 serviceWorker.unregister();
 
